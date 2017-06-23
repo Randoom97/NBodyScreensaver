9bc2b78d2535d6c506c70c7aecb80eb754a6c829
Logan <randoom97@live.com>
2017-06-22 19:25:19 -0600

    Inital setup and basic calculations

---
+++ ScreensaverTemplate/WindowsFormsApplication1/Particle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Particle
+    {
+
+        public static double singularityMass { get { return 1000.0; } }
+        public static double particleMass { get { return 1.0; } }
+
+        public double mass { get { if (singularity) { return singularityMass; } return particleMass; } }
+        public bool singularity { get; private set; }
+        public double x { get; set; }
+        public double y { get; set; }
+        public double vx { get; set; }
+        public double vy { get; set; }
+
+        public Particle(double x, double y, bool singularity)
+        {
+            this.singularity = singularity;
+            this.x = x;
+            this.y = y;
+        }
+
+        public Particle(double x, double y, double vx, double vy, bool singularity)
+        {
+            this.singularity = singularity;
+            this.x = x;
+            this.y = y;
+            this.vx = vx;
+            this.vy = vy;
+        }
+
+    }
+}
--- ScreensaverTemplate/WindowsFormsApplication1/Screensaver.Engine.cs
+++ ScreensaverTemplate/WindowsFormsApplication1/Screensaver.Engine.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace WindowsFormsApplication1
 {
@@ -9,6 +11,16 @@ namespace WindowsFormsApplication1
         // Determines the size of the internal bitmap to render to.
         // 0.5 would be half the size of the actual window.
         private double internalScale = 1.0;
+
+        //The width and height of the internal bitmap
+        private int width;
+        private int height;
+
+        private Particle[] particles;
+        private Random rand;
+        private double gconstant;
+
+        private double ptopf, stopf, stosf;
 
         /// <summary>
         /// Allows you to add fields to the settings form.
@@ -25,7 +37,23 @@ namespace WindowsFormsApplication1
         /// </summary>
         private void Setup()
         {
+            gconstant = 2.0;
+            ptopf = (gconstant * Math.Pow(Particle.particleMass, 2));
+            stopf = (gconstant * Particle.particleMass * Particle.singularityMass);
+            stosf = (gconstant * Math.Pow(Particle.singularityMass, 2));
+            width = image.Width;
+            height = image.Height;
 
+            rand = new Random();
+            particles = new Particle[100];
+            for(int i = 0; i < 3; i++)
+            {
+                particles[i] = new Particle(rand.NextDouble() * width, rand.NextDouble() * height, true);
+            }
+            for(int i = 3; i < 100; i++)
+            {
+                particles[i] = new Particle(rand.NextDouble() * width, rand.NextDouble() * height, false);
+            }
         }
 
         /// <summary>
@@ -34,7 +62,8 @@ namespace WindowsFormsApplication1
         /// </summary>
         private void Tick()
         {
-
+            gravity(particles);
+            acceleration(particles);
         }
 
         /// <summary>
@@ -45,8 +74,58 @@ namespace WindowsFormsApplication1
         /// <param name="height">Height of the drawing space</param>
         private void Render(Graphics g, int width, int height)
         {
-
+            g.FillRectangle(Brushes.Black, 0, 0, width, height);
+            SolidBrush brush = new SolidBrush(Color.FromArgb(255, 255, 0, 255));
+            foreach(Particle p in particles){
+                if (p.singularity)
+                {
+                    g.FillEllipse(brush, (int)p.x, (int)p.y, 20, 20);
+                }
+                else
+                {
+                    g.FillEllipse(brush, (int)p.x, (int)p.y, 10, 10);
+                }
+            }
         }
 
+        private void gravity(Particle[] particles)
+        {
+            double force = 0;
+            for(int i = 0; i < particles.Length-1; i++){
+                for(int j = i+1; j < particles.Length; j++)
+                {
+                    if(particles[i].singularity & particles[j].singularity)
+                    {
+                        force = stosf / distanceSquared(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
+                    }
+                    else if(!particles[i].singularity && !particles[j].singularity)
+                    {
+                        force = ptopf / distanceSquared(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
+                    }
+                    else
+                    {
+                        force = stopf / distanceSquared(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
+                    }
+                    particles[i].vx += force * ((particles[j].x - particles[i].x) / width) / particles[i].mass;
+                    particles[i].vy += force * ((particles[j].y - particles[i].y) / height) / particles[i].mass;
+                    particles[j].vx += force * ((particles[i].x - particles[j].x) / width) / particles[j].mass;
+                    particles[j].vy += force * ((particles[i].y - particles[j].y) / height) / particles[j].mass;
+                }
+            }
+        }
+
+        private void acceleration(Particle[] particles)
+        {
+            foreach(Particle p in particles)
+            {
+                p.x += p.vx;
+                p.y += p.vy;
+            }
+        }
+
+        private double distanceSquared(double x1, double y1, double x2, double y2)
+        {
+            return Math.Max((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2), double.MinValue);
+        }
     }
 }
